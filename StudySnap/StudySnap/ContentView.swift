d4ff3b63704e14fe47df69b84f61e34c6071448f
SystemFiles <bensykes12@gmail.com>
2021-03-04 12:23:58 -0500

    (init): Bootstrapped Project

---
+++ StudySnap/StudySnap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudySnap
+//
+//  Created by Ben Sykes on 2021-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Nicky!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySnap/StudySnap/StudySnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySnapApp.swift
+//  StudySnap
+//
+//  Created by Ben Sykes on 2021-03-04.
+//
+
+import SwiftUI
+
+@main
+struct StudySnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
