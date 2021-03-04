01800a913d40a2345605f4ceb5c0a4935df31083
SystemFiles <bensykes12@gmail.com>
2021-03-04 15:56:30 -0500

    (SSPP-103): Added landing page

--- StudySnap/StudySnap/ContentView.swift
+++ StudySnap/StudySnap/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Nicky!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                AppLogo()
+            }.frame(minWidth: geometry.size.width, maxWidth: .infinity, minHeight: geometry.size.height, maxHeight: .infinity, alignment: .center).background(
+                Image("LandingPageBkg").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea()
+            )
+        }
+    }
+}
+
+struct AppLogo: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            HStack(alignment: .top) {
+                VStack(spacing: 0) {
+                    Image("Logo").padding().offset(y: -200)
+                }
+            }
+        }
+    }
+}
+
+struct BtnGetStarted: View {
+    var body: some View {
+        NavigationView {
+            self.navigationTitle("")
+                .navigationBarHidden(true)
+            NavigationLink(destination: SignUpView().navigationBarHidden(true).navigationTitle("")) {
+                EmptyView()
+            }
+        }
     }
 }
 
