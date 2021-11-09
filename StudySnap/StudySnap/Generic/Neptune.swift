0ee647c1e7ace1c55bf8fcd8d33ad6e36d36b0f9
Liam Stickney <liam.0820@hotmail.com>
2021-11-09 17:48:07 -0500

    (SSPP-290): Update Note Models
    Neptune.swift, CloudNoteView.swift, NoteViewViewModel.swift and NoteSearchView.swift. For the note search view, I’ve hardcoded the rating displayed to just be [0,0,0,0,0] for now, as the ApiNoteResponse no longer contains a rating.
    
    The ApiNoteRespone no longer has rating as an attribute, and its attribute ‘body’ has been renamed ‘noteAbstract’. As such, everywhere the ApiNoteReponse was used, these changes needed to be made.

--- StudySnap/StudySnap/Generic/Neptune.swift
+++ StudySnap/StudySnap/Generic/Neptune.swift
@@ -83,7 +83,7 @@ struct ApiUserId: Codable, Identifiable{
 }
 struct ApiNoteResponse : Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
-        case title, classId, keywords, shortDescription, body, fileUri, authorId, rating, timeLength,  bibtextCitation, user, message
+        case title, classId, keywords, shortDescription, noteAbstract, fileUri, authorId, timeLength, bibtextCitation, user, message
         
         case id = "id"
     }
@@ -93,10 +93,9 @@ struct ApiNoteResponse : Codable, Identifiable {
     var classId: String?
     var keywords: [String]?
     var shortDescription: String?
-    var body: String?
+    var noteAbstract: String?
     var fileUri: String?
     var authorId: Int?
-    var rating: [Int]?
     var timeLength: Int?
     var bibtextCitation: String?
     var user: UserModel?
