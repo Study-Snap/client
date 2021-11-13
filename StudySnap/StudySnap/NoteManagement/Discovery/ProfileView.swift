95793b132dee7a0b99e52fbc74bff2abfd140657
Liam Stickney <liam.0820@hotmail.com>
2021-11-13 13:47:19 -0500

    (SSPP-172): User profile and management UI update
    Updated the profile view to show all the information in some navigation views. Also moved the buttons for logging out and changing password into their own navigation view.

--- StudySnap/StudySnap/NoteManagement/Discovery/ProfileView.swift
+++ StudySnap/StudySnap/NoteManagement/Discovery/ProfileView.swift
@@ -21,107 +21,71 @@ struct ProfileView: View {
         NavigationView {
             ZStack {
                 VStack {
-                    HStack (alignment: .center) {
-                        Text("My Profile")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(.top, -50)
-                    }
-                    
                     if viewModel.response == nil || viewModel.userClassroomResponse == nil {
                         // Loading still
                         ProgressView("Loading user profile")
                             .foregroundColor(Color("Secondary"))
                     } else {
-                        VStack(alignment: .leading) {
-                            VStack(alignment: .leading) {
-                                Text("Full Name")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                Text("\((viewModel.response?.firstName)!) \((viewModel.response?.lastName)!)").fontWeight(.light)
-                                    .font(.system(size: 20))
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Email")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                Text((viewModel.response?.email)!).fontWeight(.light)
-                                    .font(.system(size: 20))
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Unique ID")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                let idString = String((viewModel.response?.id)!)
-                                Text(idString).fontWeight(.light)
-                                    .font(.system(size: 20))
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Number of Classrooms")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                Text(String((viewModel.userClassroomResponse?.count) ?? 0)).fontWeight(.light)
-                                    .font(.system(size: 20))
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Number of Uploaded Notes")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                Text(String((viewModel.userNoteResponse?.count) ?? 0)).fontWeight(.light)
-                                    .font(.system(size: 20))
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Most Recent Upload")
-                                    .foregroundColor(Color("Primary"))
-                                    .font(.system(size: 15))
-                                NoteListRowItem(id: viewModel.userNoteResponse?[0].id ?? 0, title: viewModel.userNoteResponse?[0].title ?? "No note found.", author: "\((viewModel.response?.firstName)!) \((viewModel.response?.lastName)!)", shortDescription: viewModel.userNoteResponse?[0].shortDescription ?? "No note found.", readTime: viewModel.userNoteResponse?[0].timeLength ?? 0, rating: [0,0,0,0])
-                            }.padding(.vertical, 3)
-                                .padding(.leading, 5)
-                        }.frame(
-                            maxWidth: .infinity,
-                            maxHeight: .infinity,
-                            alignment: .topLeading
-                        )
+                        NavigationView {
+                            List {
+                                Section {
+                                    Text("Full Name")
+                                        .foregroundColor(Color("Primary"))
+                                        .font(.system(size: 15))
+                                    Text("\((viewModel.response?.firstName)!) \((viewModel.response?.lastName)!)").fontWeight(.light)
+                                        .font(.system(size: 20))
+                                    Text("Email")
+                                        .foregroundColor(Color("Primary"))
+                                        .font(.system(size: 15))
+                                    Text((viewModel.response?.email)!).fontWeight(.light)
+                                        .font(.system(size: 20))
+                                    Text("Unique ID")
+                                        .foregroundColor(Color("Primary"))
+                                        .font(.system(size: 15))
+                                    let idString = String((viewModel.response?.id)!)
+                                    Text(idString).fontWeight(.light)
+                                        .font(.system(size: 20))
+                                    Text("Number of Classrooms")
+                                        .foregroundColor(Color("Primary"))
+                                        .font(.system(size: 15))
+                                    Text(String((viewModel.userClassroomResponse?.count) ?? 0)).fontWeight(.light)
+                                        .font(.system(size: 20))
+                                    Text("Number of Uploaded Notes")
+                                        .foregroundColor(Color("Primary"))
+                                        .font(.system(size: 15))
+                                    Text(String((viewModel.userNoteResponse?.count) ?? 0)).fontWeight(.light)
+                                        .font(.system(size: 20))
+                                }.accentColor(.primary)
+                                    .listRowSeparatorTint(.blue)
+                                    .listRowSeparator(.hidden)
+                            }.listStyle(.insetGrouped)
+                             .navigationTitle("My Profile")
+                        }.navigationBarTitle("")
+                        .navigationBarHidden(true)
                     }
+
                     
-                    Spacer()
-                    
-                    // TODO: Add functionality to change password button
-                    PrimaryButtonView(title: "Change Password", action: {
-                        print("Implement change password")
-                    }).alert("Error", isPresented: $error) {
-                        Button("Ok", role: .cancel) {
-                            self.error = false
-                        }
-                    } message: {
-                        Text("Error changing password.")
-                    }
-                    
-                    PrimaryButtonView(title: "Log Out", action: {
-                        self.viewModel.performLogout()
-                        if (self.viewModel.logout) {
-                            self.rootIsActive = false
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                    }).alert("Error", isPresented: $error) {
-                        Button("Ok", role: .cancel) {
-                            self.error = false
-                        }
-                    } message: {
-                        Text("Error signing out.")
-                    }
-                    
-                    Spacer()
+                    NavigationView {
+                        List {
+                            Section {
+                                // TODO: Implement change password
+                                Button("Change Password") {
+                                    print("Implement change password")
+                                }.buttonStyle(BorderlessButtonStyle())
+                                
+                                Button("Log Out") {
+                                    self.viewModel.performLogout()
+                                    if (self.viewModel.logout) {
+                                        self.rootIsActive = false
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }
+                                }.buttonStyle(BorderlessButtonStyle())
+                            }.accentColor(.primary)
+                        }.listStyle(.insetGrouped)
+                         .navigationTitle("Settings")
+                    }.navigationBarTitle("")
+                        .navigationBarHidden(true)
+                        .frame(maxHeight: .infinity, alignment: .bottom)
                 }
             }
         }.onAppear(perform: {
