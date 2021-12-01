01e9e4d5a4446a5c83f641487224a8e79d415b02
SystemFiles <bensykes12@gmail.com>
2021-12-01 12:41:09 -0500

    (SSPP-307): Added loading indicators where needed + UI enhancements

--- StudySnap/StudySnap/ClassroomManagement/Create Classroom/CreateClassroomView.swift
+++ StudySnap/StudySnap/ClassroomManagement/Create Classroom/CreateClassroomView.swift
@@ -17,67 +17,75 @@ struct CreateClassroomView: View {
     @State private var isShowingPhotoPicker = false
     @State private var classThumbnailImage = UIImage(named: "classthumbnail")!
     @State private var incompleteEntry = false
+    @State private var uploading: Bool = false
     
     @Binding var isClassroomsUpdated: Bool
     
     var body: some View {
         NavigationView {
-            VStack {
-                InputField(fieldHeight: 15, textStyle: .emailAddress, autoCap: false, placeholder: "Enter a classroom name", value: $viewModel.name).padding(.top, 20).padding(.bottom, 10).padding(.horizontal, 17.5)
+            if (uploading && !self.viewModel.error) {
+                ProgressView("Creating the classroom!")
+                    .foregroundColor(Color("Secondary"))
+            } else {
                 VStack {
+                    InputField(fieldHeight: 15, textStyle: .emailAddress, autoCap: false, placeholder: "Enter a classroom name", value: $viewModel.name).padding(.top, 20).padding(.bottom, 10).padding(.horizontal, 17.5)
                     VStack {
-                        Image(uiImage: classThumbnailImage)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 300, height: 300)
-                            .clipShape(Rectangle())
-                            .cornerRadius(12)
-                            .padding(.horizontal)
-                        .padding(.top)
-                        
-                        Text("Pick Image from Photo album")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .padding(.bottom)
-                    }
-                    .onTapGesture {
-                        isShowingPhotoPicker = true
-                    }
-                }
-                .sheet(isPresented: $isShowingPhotoPicker, content: {
-                    PhotoPicker(classThumbnailImage: $classThumbnailImage)
-                })
-                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 12, style: .continuous))
-                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x:0, y: 5)
-                .strokeStyle()
-
-
-                Spacer()
-                PrimaryButtonView(title:"Create", action: {
-                    if !viewModel.name.isEmpty{
-                        self.viewModel.createClassroom(classCreateData: CreateClassroomData(name: viewModel.name, thumbnailData: classThumbnailImage.pngData())) {
-                            if self.viewModel.unauthorized {
-                                // Refresh failed, return to login
-                                self.rootIsActive = false
-                            } else {
-                                self.isClassroomsUpdated = true // Force refresh on classroom dashboard when classroom is created
-                            }
-                            presentationMode.wrappedValue.dismiss()
+                        VStack {
+                            Image(uiImage: classThumbnailImage)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 300, height: 300)
+                                .clipShape(Rectangle())
+                                .cornerRadius(12)
+                                .padding(.horizontal)
+                            .padding(.top)
+                            
+                            Text("Pick Image from Photo album")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding(.bottom)
                         }
-                    }else{
-                        self.incompleteEntry = true
+                        .onTapGesture {
+                            isShowingPhotoPicker = true
+                        }
                     }
-                    
-                })
-                .padding()
-                .alert("Error: Missing information", isPresented: $incompleteEntry) {
-                    Button("Ok", role: .cancel){
-                        self.incompleteEntry = false
+                    .sheet(isPresented: $isShowingPhotoPicker, content: {
+                        PhotoPicker(classThumbnailImage: $classThumbnailImage)
+                    })
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 12, style: .continuous))
+                    .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x:0, y: 5)
+                    .strokeStyle()
+
+
+                    Spacer()
+                    PrimaryButtonView(title:"Create", action: {
+                        self.uploading = true
+                        if !viewModel.name.isEmpty{
+                            self.viewModel.createClassroom(classCreateData: CreateClassroomData(name: viewModel.name, thumbnailData: classThumbnailImage.pngData())) {
+                                self.uploading = false
+                                if self.viewModel.unauthorized {
+                                    // Refresh failed, return to login
+                                    self.rootIsActive = false
+                                } else {
+                                    self.isClassroomsUpdated = true // Force refresh on classroom dashboard when classroom is created
+                                }
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        }else{
+                            self.incompleteEntry = true
+                        }
+                        
+                    })
+                    .padding()
+                    .alert("Error: Missing information", isPresented: $incompleteEntry) {
+                        Button("Ok", role: .cancel){
+                            self.incompleteEntry = false
+                        }
+                    } message:{
+                        Text("Please enter a class name")
                     }
-                } message:{
-                    Text("Please enter a class name")
-                }
-            }.navigationBarTitle("Create Classroom", displayMode: .inline)
+                }.navigationBarTitle("Create Classroom", displayMode: .inline)
+            }
         }
     }
 }
