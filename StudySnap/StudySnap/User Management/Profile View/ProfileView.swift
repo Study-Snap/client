9fee3f69327f597c1226828f8c9a8dfcb1db25c6
SystemFiles <bensykes12@gmail.com>
2021-12-09 09:53:19 -0500

    (SSPP-323): Implemented fix for profile authentication

--- StudySnap/StudySnap/User Management/Profile View/ProfileView.swift
+++ StudySnap/StudySnap/User Management/Profile View/ProfileView.swift
@@ -59,7 +59,7 @@ struct ProfileView: View {
                                 Section {
                                     HStack{
                                         VStack{
-                                            Text("\(self.viewModel.userNotesCount ?? 12)")
+                                            Text("\(self.viewModel.userNotesCount ?? 0)")
                                                 .padding(.vertical, 10)
                                                 .font(.title2)
                                                 .foregroundColor(Color("BrightPrimaryConstant"))
@@ -71,7 +71,7 @@ struct ProfileView: View {
                                         .padding()
                                         Divider()
                                         VStack{
-                                            Text("\(self.viewModel.userClassroomCount ?? 3)")
+                                            Text("\(self.viewModel.userClassroomCount ?? 0)")
                                                 .padding(.vertical, 10)
                                                 .font(.title2)
                                                 .foregroundColor(Color("BrightPrimaryConstant"))
@@ -86,7 +86,7 @@ struct ProfileView: View {
                                 Section {
                                     VStack(alignment: .center){
                                         VStack{
-                                            Text("\(self.viewModel.userTotalContentMinutes ?? 73)")
+                                            Text("\(self.viewModel.userTotalContentMinutes ?? 0)")
                                                 .padding(.vertical, 10)
                                                 .font(.title2)
                                                 .foregroundColor(Color("BrightPrimaryConstant"))
@@ -141,67 +141,32 @@ struct ProfileView: View {
 
             }.navigationBarHidden(true)
         }.onAppear(perform: {
-            self.viewModel.getUserInformation() {
+            self.viewModel.getUserInformation {
                 if self.viewModel.unauthorized {
-                    // If we cannot refresh, pop off back to login
+                    // Refresh failed, return to login
                     self.rootIsActive = false
                 }
-                if self.viewModel.error {
-                    // Unknown error getting user data -- logout user
-                    self.viewModel.performLogout()
-                    if (self.viewModel.logout) {
+                
+                self.viewModel.getTotalContentMinutes {
+                    if self.viewModel.unauthorized {
+                        // Refresh failed, return to login
                         self.rootIsActive = false
-                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    
+                    self.viewModel.getTotalNotesByUser {
+                        if self.viewModel.unauthorized {
+                            // Refresh failed, return to login
+                            self.rootIsActive = false
+                        }
+                        
+                        self.viewModel.getClassroomsCount {
+                            if self.viewModel.unauthorized {
+                                // Refresh failed, return to login
+                                self.rootIsActive = false
+                            }
+                        }
                     }
                 }
-
-//                // Load user classrooms
-//                self.viewModel.getClassroomsCount {
-//                    if self.viewModel.unauthorized {
-//                        // If we cannot refresh, pop off back to login
-//                        self.rootIsActive = false
-//                    }
-//                    if self.viewModel.error {
-//                        // Unknown error getting user data -- logout user
-//                        self.viewModel.performLogout()
-//                        if (self.viewModel.logout) {
-//                            self.rootIsActive = false
-//                            self.presentationMode.wrappedValue.dismiss()
-//                        }
-//                    }
-//
-//                    // Load user published notes
-//                    self.viewModel.getTotalNotesByUser {
-//                        if self.viewModel.unauthorized {
-//                            // If we cannot refresh, pop off back to login
-//                            self.rootIsActive = false
-//                        }
-//                        if self.viewModel.error {
-//                            // Unknown error getting user data -- logout user
-//                            self.viewModel.performLogout()
-//                            if (self.viewModel.logout) {
-//                                self.rootIsActive = false
-//                                self.presentationMode.wrappedValue.dismiss()
-//                            }
-//                        }
-//
-//                        // Load user content length (minutes)
-//                        self.viewModel.getTotalContentMinutes {
-//                            if self.viewModel.unauthorized {
-//                                // If we cannot refresh, pop off back to login
-//                                self.rootIsActive = false
-//                            }
-//                            if self.viewModel.error {
-//                                // Unknown error getting user data -- logout user
-//                                self.viewModel.performLogout()
-//                                if (self.viewModel.logout) {
-//                                    self.rootIsActive = false
-//                                    self.presentationMode.wrappedValue.dismiss()
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
             }
         })
     }
